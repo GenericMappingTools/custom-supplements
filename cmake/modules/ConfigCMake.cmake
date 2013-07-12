9852afd9b59480cf99525b181ce4333f77b173d5
Paul Wessel <pwessel@hawaii.edu>
2013-07-12 16:33:14 +0000

    Finally a plugin module

--- cmake/modules/ConfigCMake.cmake
+++ cmake/modules/ConfigCMake.cmake
@@ -42,49 +42,6 @@ if (CMAKE_BUILD_TYPE MATCHES "Debug|RelWithDebInfo")
 	set (DEBUG_BUILD TRUE)
 endif (CMAKE_BUILD_TYPE MATCHES "Debug|RelWithDebInfo")
 
-# Here we change it to add the SVN revision number for non-public releases - see Package.cmake for
-# why this has to be done here.
-set (CUSTOM_PACKAGE_VERSION_WITH_SVN_REVISION ${CUSTOM_PACKAGE_VERSION})
-# Add the Subversion version number to the package filename if this is a non-public release.
-# A non-public release has an empty 'CUSTOM_SOURCE_CODE_CONTROL_VERSION_STRING' variable in 'ConfigDefault.cmake'.
-set (HAVE_SVN_VERSION)
-if (NOT CUSTOM_SOURCE_CODE_CONTROL_VERSION_STRING)
-	# Get the location, inside the staging area location, to copy the application bundle to.
-	execute_process (
-		COMMAND svnversion ${CUSTOM_SOURCE_DIR}
-		RESULT_VARIABLE SVN_VERSION_RESULT
-		OUTPUT_VARIABLE SVN_VERSION_OUTPUT
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-	if (SVN_VERSION_RESULT)
-		message (STATUS "Unable to determine svn version number for non-public release - ignoring.")
-	else (SVN_VERSION_RESULT)
-		if (SVN_VERSION_OUTPUT MATCHES "Unversioned")
-			message (STATUS "Unversioned source tree, non-public release.")
-		else (SVN_VERSION_OUTPUT MATCHES "Unversioned")
-			# The 'svnversion' command can output a range of revisions with a colon
-			# separator - but this causes problems with filenames so we'll remove the
-			# colon and the end revision after it.
-			string (REGEX REPLACE ":.*$" "" SVN_VERSION ${SVN_VERSION_OUTPUT})
-			if (NOT SVN_VERSION STREQUAL exported)
-				# Set the updated package version.
-				set (CUSTOM_PACKAGE_VERSION_WITH_SVN_REVISION "${CUSTOM_PACKAGE_VERSION}_r${SVN_VERSION}")
-				set (HAVE_SVN_VERSION TRUE)
-			endif (NOT SVN_VERSION STREQUAL exported)
-		endif (SVN_VERSION_OUTPUT MATCHES "Unversioned")
-	endif (SVN_VERSION_RESULT)
-endif (NOT CUSTOM_SOURCE_CODE_CONTROL_VERSION_STRING)
-
-# The current CUSTOM version.
-set (CUSTOM_VERSION_STRING "${CUSTOM_PACKAGE_NAME} ${CUSTOM_PACKAGE_VERSION_WITH_SVN_REVISION}")
-
-set (CUSTOM_LONG_VERSION_STRING "${CUSTOM_PACKAGE_NAME} - ${CUSTOM_PACKAGE_DESCRIPTION_SUMMARY}, Version ${CUSTOM_PACKAGE_VERSION_WITH_SVN_REVISION}")
-
-# Set install name suffix used for directories and main gmt executable
-if (NOT DEFINED CUSTOM_INSTALL_NAME_SUFFIX)
-	set (CUSTOM_INSTALL_NAME_SUFFIX "-${CUSTOM_PACKAGE_VERSION_WITH_SVN_REVISION}")
-endif (NOT DEFINED CUSTOM_INSTALL_NAME_SUFFIX)
-
 # Get date
 try_run (_exit_today _compiled_today
 	${CMAKE_BINARY_DIR}/CMakeTmp
@@ -105,11 +62,6 @@ list(GET _today 0 1 2 DATE)
 string (REPLACE ";" "-" DATE "${DATE}")
 set (_today)
 
-# set package date
-if (NOT CUSTOM_VERSION_YEAR)
-	set (CUSTOM_VERSION_YEAR ${YEAR})
-endif (NOT CUSTOM_VERSION_YEAR)
-
 # reset list of extra license files
 set (CUSTOM_EXTRA_LICENSE_FILES)
 
@@ -120,13 +72,9 @@ set (COPYING_LGPL ${CUSTOM_SOURCE_DIR}/COPYING.LESSERv3)
 # Install path for CUSTOM binaries, headers and libraries
 include (GNUInstallDirs) # defines CMAKE_INSTALL_LIBDIR (lib/lib64)
 if (CUSTOM_INSTALL_MONOLITHIC)
-	set (CUSTOM_INCDIR include)
 	set (CUSTOM_LIBDIR ${CMAKE_INSTALL_LIBDIR})
-	set (CUSTOM_BINDIR bin)
 else (CUSTOM_INSTALL_MONOLITHIC)
-	set (CUSTOM_INCDIR include/gmt${CUSTOM_INSTALL_NAME_SUFFIX})
 	set (CUSTOM_LIBDIR ${CMAKE_INSTALL_LIBDIR}/gmt${CUSTOM_INSTALL_NAME_SUFFIX}/lib)
-	set (CUSTOM_BINDIR ${CMAKE_INSTALL_LIBDIR}/gmt${CUSTOM_INSTALL_NAME_SUFFIX}/bin)
 endif (CUSTOM_INSTALL_MONOLITHIC)
 
 # use, i.e. don't skip the full RPATH for the build tree
@@ -140,19 +88,19 @@ set (CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 set (CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${CUSTOM_LIBDIR}")
 
 # make executables relocatable on supported platforms
-if (UNIX AND NOT CYGWIN)
-	# find relative libdir from executable dir
-	file (RELATIVE_PATH _rpath /${CUSTOM_BINDIR} /${CUSTOM_LIBDIR})
-	# remove trailing /
-	string (REGEX REPLACE "/$" "" _rpath "${_rpath}")
-	if (APPLE)
-		# relative RPATH on osx
-		set (CMAKE_INSTALL_NAME_DIR @loader_path/${_rpath})
-	else (APPLE)
-		# relative RPATH on Linux, Solaris, etc.
-		set (CMAKE_INSTALL_RPATH "\$ORIGIN/${_rpath}")
-	endif (APPLE)
-endif (UNIX AND NOT CYGWIN)
+#if (UNIX AND NOT CYGWIN)
+#	# find relative libdir from executable dir
+#	file (RELATIVE_PATH _rpath /${CUSTOM_BINDIR} /${CUSTOM_LIBDIR})
+#	# remove trailing /
+#	string (REGEX REPLACE "/$" "" _rpath "${_rpath}")
+#	if (APPLE)
+#		# relative RPATH on osx
+#		set (CMAKE_INSTALL_NAME_DIR @loader_path/${_rpath})
+#	else (APPLE)
+#		# relative RPATH on Linux, Solaris, etc.
+#		set (CMAKE_INSTALL_RPATH "\$ORIGIN/${_rpath}")
+#	endif (APPLE)
+#endif (UNIX AND NOT CYGWIN)
 
 # add the automatically determined parts of the RPATH
 # which point to directories outside the build tree to the install RPATH
